b9c78266187c41bcd28c54d6ed31a6339dcc90f7
mfakane <509998+mfakane@users.noreply.github.com>
2023-03-21 20:20:25 +0900

    Add example for digitizers

---
+++ RawInput.Sharp.DigitizerExample/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace RawInput.Sharp.DigitizerExample;
+
+partial class MainForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.textLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.AutoSize = true;
+            this.textLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textLabel.Location = new System.Drawing.Point(0, 0);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(38, 15);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.Text = "Touch anywhere in this window.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textLabel);
+            this.Name = "MainForm";
+            this.Text = "Digitizer Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private Label textLabel;
+}
